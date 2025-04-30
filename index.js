ac2d17623988639c735adcdf552fd03d1498c017
Muhammad Kasim Naina Mohammed <muhammadkasim@gmail.com>
2025-04-30 19:19:59 -0400

    webdeveloper portfolio

---
+++ index.js
@@ -0,0 +1,63 @@
+// template_x322yem
+// service_ezlefgq
+// 0sONcY2n33ioZWCjM
+let isModalOpen=false;
+let contrastToggle=false
+const scaleFactor=1/20;
+
+
+function moveBackground(event){
+    const shapes=document.querySelectorAll(".shape")
+    const x=event.clientX*scaleFactor;
+    const y=event.clientY*scaleFactor;
+    for(let i=0;i<shapes.length;i++){
+        const isOdd=i%2!==0;
+        const booleanInteger=isOdd? -1:1;
+        shapes[i].style.transform=`translate(${x*booleanInteger}px,${y*booleanInteger}px)`
+    }
+}
+
+function toggleTheme(){
+    contrastToggle=!contrastToggle
+    if(contrastToggle){
+        document.body.classList+=(" dark-theme")
+    }
+    else{
+        document.body.classList.remove("dark-theme")
+    }
+    document.body.classList.toggle("dark-theme")
+    document.body.classList.toggle("light-theme")
+}
+
+
+function contact(event){
+    event.preventDefault()
+    const loading=document.querySelector(".modal__overlay--loading")
+    const success= document.querySelector (".modal__overlay--success")
+    loading.classList+=("  modal__overlay--visible")
+    emailjs.sendForm(
+        'service_ezlefgq', 
+        'template_x322yem',
+        event.target,
+        '0sONcY2n33ioZWCjM')
+        .then(()=> {
+            loading.classList.remove("modal__overlay--visible")
+            success.classList+=(" modal__overlay--visible")
+        })
+        .catch(()=>{
+            loading.classList.remove("modal__overlay--visible")
+            alert("Failed to send message, please try again, Please contact me directly @muhammadkasim@gmail.com")
+        })
+}
+
+
+
+function toggleModal(){
+    if(isModalOpen){
+        isModalOpen=false;
+        return document.body.classList.remove("modal--open")
+    }
+    isModalOpen=true;
+    document.body.classList+= " modal--open";
+}
+
